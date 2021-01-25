c82e2f708425019fe6a92353bbf9b2eb0d66fb10
steven <steven.h@live.com>
2021-01-25 13:40:06 -0800

    add post and foreign key column

--- db/migrate/20210125203929_create_posts.rb
+++ db/migrate/20210125203929_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.text :post
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
