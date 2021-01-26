e8cc95f87f42d83d4de44c5ac68eedefdd3d604f
steven <steven.h@live.com>
2021-01-25 17:03:12 -0800

    posts are now able to be posted when logged in.
    params includes the current user id.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,17 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit :post, :user_id
   end
 end
