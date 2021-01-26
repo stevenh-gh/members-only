ecdad898f88bfccc959d211fa4efdd97632ce940
steven <steven.h@live.com>
2021-01-25 19:17:28 -0800

    able to login and create posts. users not logged in cannot view post's author.
    add login, sign up, logout and home links to all pages.
    respective links will not appear on their respective pages (ex, home link will not appear in home page)
    
    connected user to registrations controller in routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
